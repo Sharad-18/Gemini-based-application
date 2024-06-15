d5bd44298fcbab9f13763e19b816c4a645fb7b1b
Sharad Sisodiya <92092443+Sharad-18@users.noreply.github.com>
2024-06-15 15:23:30 +0530

    Add files via upload

---
+++ math_gesture/main.py
@@ -0,0 +1,85 @@
+import cvzone
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import google.generativeai as genai
+from PIL import Image
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([3, 2])
+with col1:
+    run = st.checkbox('Run', value=True)
+    FRAME_WINDOW = st.image([])
+
+with col2:
+    st.title("Answer")
+    output_text_area = st.subheader("")
+
+genai.configure(api_key="AIzaSyA5KGbUSklccRWcr0FSgfFmlCQR_M7I6ww")
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
+
+def getHandInfo(img):
+    hands, img = detector.findHands(img, draw=False, flipType=True)
+    if hands:
+        hand = hands[0]
+        lmList = hand["lmList"]
+        fingers = detector.fingersUp(hand)
+        print(fingers)
+        return fingers, lmList
+    else:
+        return None
+
+def draw(info, prev_pos, canvas):
+    fingers, lmList = info
+    current_pos = None
+    if fingers == [0, 1, 0, 0, 0]:
+        current_pos = lmList[8][0:2]
+        if prev_pos is None: prev_pos = current_pos
+        cv2.line(canvas, tuple(current_pos), tuple(prev_pos), (255, 0, 255), 10)
+    elif fingers == [0, 0, 0, 0, 1]:
+        canvas = np.zeros_like(img)
+    return current_pos, canvas
+
+def sendToAI(model, canvas, fingers):
+    if fingers == [1, 1, 1, 1, 0]:
+        pil_image = Image.fromarray(canvas)
+        response = model.generate_content(["Solve this math problem", pil_image])
+        return response.text
+
+prev_pos = None
+canvas = None
+image_combined = None
+output_text = ""
+
+while run:
+    success, img = cap.read()
+    if not success:
+        st.warning("Failed to capture image from webcam. Please check your webcam settings.")
+        continue
+
+    img = cv2.flip(img, 1)
+    
+    if canvas is None:
+        canvas = np.zeros_like(img)
+    
+    info = getHandInfo(img)
+    if info:
+        fingers, lmList = info
+        prev_pos, canvas = draw(info, prev_pos, canvas)
+        output_text = sendToAI(model, canvas, fingers)
+
+    image_combined = cv2.addWeighted(img, 0.7, canvas, 0.3, 0)
+    FRAME_WINDOW.image(image_combined, channels="BGR")
+
+    if output_text:
+        output_text_area.text(output_text)
+
+    cv2.waitKey(1)
---
+++ youtube_summery_gen/app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+import google.generativeai as genai
+import os
+from youtube_transcript_api import YouTubeTranscriptApi
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+prompt="""You are Yotube video summarizer. You will be taking the transcript text
+and summarizing the entire video and providing the important summary in points
+ 350 and more  words and also you need to add some necessary content according to the summery 
+ like definition of the topic with its core concept or some imporatant terms and i will use this
+ content in my blog so content should be according to the blog post. Please provide the 
+ summary of the text given here:  """
+
+def extract_transcript_details(youtube_video_url):
+    try:
+        video_id=youtube_video_url.split("=")[1]
+        
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript = ""
+        for i in transcript_text:
+            transcript += " " + i["text"]
+
+        return transcript
+
+    except Exception as e:
+        raise e
+    
+## getting the summary based on Prompt from Google Gemini Pro
+def generate_gemini_content(transcript_text,prompt):
+
+    model=genai.GenerativeModel("gemini-pro")
+    response=model.generate_content(prompt+transcript_text)
+    return response.text
+
+st.title("YouTube Transcript to Detailed Notes Converter")
+youtube_link = st.text_input("Enter YouTube Video Link:")
+
+if youtube_link:
+    video_id = youtube_link.split("=")[1]
+    print(video_id)
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+
+if st.button("Get Detailed Notes"):
+    transcript_text=extract_transcript_details(youtube_link)
+
+    if transcript_text:
+        summary=generate_gemini_content(transcript_text,prompt)
+        st.markdown("## Detailed Notes:")
+        st.write(summary)
